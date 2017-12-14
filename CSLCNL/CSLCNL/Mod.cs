c125654f50356df70e14bab9130b4dac3186b25b
Markus Rezai <markus.rezai@gmail.com>
2017-12-14 05:50:48 +0100

    Added mod class

---
+++ CSLCNL/CSLCNL/Mod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ICities;
+using UnityEngine;
+
+namespace Makaki.CustomNameLists
+{
+    public class Mod : IUserMod
+    {
+        public string Name => "Custom Name Lists";
+        public string Description => "Provides a framework to add namelists for cities, districts, streets, buildings and much more.";
+    }
+}
